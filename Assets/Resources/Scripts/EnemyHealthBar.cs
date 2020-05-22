44d9da9c26d9583653b9c31b3e186cf8a3b6bc23
FIRST_NAME LAST_NAME <andre.oikawa@hotmail.com>
2020-05-22 14:16:28 -0400

    health bar on enemies

---
+++ Assets/Resources/Scripts/EnemyHealthBar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    private void LateUpdate() {
+        
+        transform.LookAt(GameObject.Find("/Player/Camera").transform.forward + transform.position);
+        // transform.Rotate(0,180,0);
+    }
+}
