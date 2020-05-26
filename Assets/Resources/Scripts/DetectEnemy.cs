d2689bdbb1ea145c303a79fffbf90f824a9269a7
FIRST_NAME LAST_NAME <andre.oikawa@hotmail.com>
2020-05-25 23:02:43 -0400

    tower shooting started

---
+++ Assets/Resources/Scripts/DetectEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectEnemy : MonoBehaviour
+{
+    public Transform GetClosestEnemy(Vector3 position) {
+        int numChildren = transform.childCount;
+        if (numChildren == 0) return null;
+        Transform retval = transform.GetChild(0);
+        float minDist = (position - retval.position).magnitude;
+        for (int i = 1; i < numChildren; i++) {
+            Transform curr = transform.GetChild(i);
+            float currDist = (position - curr.position).magnitude;
+            if (minDist > currDist) {
+                retval = curr;
+                minDist = currDist;
+            }
+        }
+
+        return retval;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Resources/Scripts/TowerShoot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerShoot : MonoBehaviour
+{
+    [SerializeField]
+    private float attackSpeed = 1f;
+    [SerializeField]
+    private float attackRadius = 5f;
+    [SerializeField]
+    private float projectileSpeed = 10f;
+    [SerializeField]
+    private GameObject projectileObj;
+    private Transform target;
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Shoot() {
+        var shootPosition = transform.position + new Vector3(0,5,0);
+        var projectile = Instantiate(projectileObj, shootPosition, Quaternion.identity);
+        projectile.GetComponent<Rigidbody>().AddForce((target.position - shootPosition) * projectileSpeed);
+        projectile.transform.parent = GameObject.Find("Projectiles").transform;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        target = GameObject.Find("Enemies").GetComponent<DetectEnemy>().GetClosestEnemy(transform.position);
+        if (target != null && time >= attackSpeed) {
+            Shoot();
+            time = 0f;
+        }
+    }
+}
